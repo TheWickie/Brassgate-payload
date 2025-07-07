efce1b9b9b06665a625943303042a41823ca2b19
TheWickie <mrcrisrobson@gmail.com>
2025-07-07 10:53:44 +0100

    Create GeneratedPosts.ts

---
+++ src/payload/collections/GeneratedPosts.ts
@@ -0,0 +1,52 @@
+import { CollectionConfig } from 'payload/types'
+
+const GeneratedPosts: CollectionConfig = {
+  slug: 'generated-posts',
+  admin: {
+    useAsTitle: 'platform',
+    defaultColumns: ['platform', 'theme', 'approved'],
+  },
+  fields: [
+    {
+      name: 'platform',
+      type: 'select',
+      options: [
+        'Twitter', 'Instagram', 'Facebook', 'LinkedIn', 'Discord',
+        'Bluesky', 'TikTok', 'YouTube', 'Email', 'Blog', 'Article'
+      ],
+      required: true,
+    },
+    {
+      name: 'theme',
+      type: 'text',
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+    },
+    {
+      name: 'caption',
+      type: 'text',
+    },
+    {
+      name: 'content',
+      type: 'textarea',
+      admin: {
+        rows: 10,
+      },
+    },
+    {
+      name: 'approved',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+    {
+      name: 'sentToN8n',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+  ],
+}
+
+export default GeneratedPosts
